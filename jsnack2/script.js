a0f38b4f5defb3b6d263c611fa4ee5deb5498355
Maccio92 <valerio362@gmail.com>
2021-11-26 17:55:50 +0100

    add while

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -5,14 +5,26 @@ const list = ['aldo', 'giovanni', 'giacomo', 'marina', 'claudio', 'laura'];
 let guest = prompt("Inserisci il tuo nome:");
 
 let find = false;
-for (let index = 0; index < list.length; index++) {
-    let nameList = list[index];
-    if (guest.toLowerCase() == nameList.toLowerCase()) {
-      find = true;
-    }  
+// Ciclo For
+// for (let index = 0; index < list.length; index++) {
+//     let nameList = list[index];
+//     if (guest.toLowerCase() == nameList.toLowerCase()) {
+//       find = true;
+//     }  
+// }
+
+// Ciclo While
+let index = 0;
+while (index < list.length && find == false){
+    if (guest.toLowerCase() == list[index].toLowerCase()) {
+    find = true;
+    }
+    index++;
 }
+
+
 if (find == true) {
     console.log ( 'puoi entrare');
-}else{
+} else{
     console.log ( 'Non puoi entrare');
 }
